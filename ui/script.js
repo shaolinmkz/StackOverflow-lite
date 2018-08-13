6a2bc3daa905e396fd2e9114fbb5db0a017be037
Nwabuzor C. Obiora <nwabuzor.obiora@gmail.com>
2018-08-13 11:27:49 +0100

    refactored the script file

--- ui/script.js
+++ ui/script.js
@@ -122,7 +122,55 @@ const postUsersQuestion1 = () => {
     paragraph.style.marginBottom = "1.5em";
 /*--------------------------------------------------------------------------------------*/
     label.appendChild(anchor); //Label appends the anchor
+
 /*--------------------------------------------------------------------------------------*/
+/*-----------------------------------------COMMENT--------------------------------------*/
+
+    let divc = document.createElement("div");
+    let _classc = document.createAttribute("class");
+    _classc.value = "comment-main-container";
+    divc.setAttributeNode(_classc);
+
+    let imgc = document.createElement("img");
+    let srcc = document.createAttribute("src");
+    srcc.value = "images/avatar.png";
+    imgc.setAttributeNode(srcc);
+
+    let _class1c = document.createAttribute("class");
+    _class1c.value = "reply-avatar";
+    imgc.setAttributeNode(_class1c);
+
+    let labelc = document.createElement("label");
+
+    let anchorc = document.createElement("a");
+    let linkc = document.createAttribute("href");
+    linkc.value = "#";
+    anchorc.setAttributeNode(linkc);
+
+    let _class2c = document.createAttribute("class");
+    _class2c.value = "reply-name";
+    anchorc.setAttributeNode(_class2c);
+
+    let textc = document.createTextNode("C.V");
+    anchorc.appendChild(textc);   //appended V.T to anchor
+    labelc.appendChild(anchorc); //appended anchor to label
+
+    let textAreac = document.createElement("textarea");
+    let placeHolderc = document.createAttribute("placeholder");
+    placeHolderc.value = "Write a comment...";
+    textAreac.setAttributeNode(placeHolderc);
+
+    let _class3c = document.createAttribute("class");
+    _class3c.value = "reply-text";
+    textAreac.setAttributeNode(_class3c);
+
+    divc.appendChild(imgc);
+    divc.appendChild(labelc);
+    divc.appendChild(textAreac);
+
+/*--------------------------------------------------------------------------------------*/
+/*--------------------------------------------------------------------------------------*/
+
 //Appends all child elements of article... 
     article.appendChild(img);
     article.appendChild(label);
@@ -130,8 +178,13 @@ const postUsersQuestion1 = () => {
     article.appendChild(input);
     article.appendChild(input2);
     article.appendChild(input3);
+    article.appendChild(document.createElement("hr"));
+    article.appendChild(divc);
+
+    homeFeeds.insertBefore(article, homeFeeds.firstChild); //Insert the post/article inside the div but before the div's first child
+    inputQuestion.value = ""; //clears the post field after posting
     
-/*------------------------------FORMAT OF ARRANGMENT------------------------------------- 
+/*--------------------------------------FORMAT OF ARRANGMENT------------------------------------- 
     `<article class="my-post">
         <img src="images/avatar.png" alt="profile-avatar" title="other-users-avatar" class="other-users-avatar" />
         <label>
@@ -141,11 +194,18 @@ const postUsersQuestion1 = () => {
             ${inputQuestion.value}
         </p>
         <input type="button" value="Delete" class="delete-question" id="delete-button"/>
+
+        <div class="comment-main-container">
+            <img src="images/avatar.png" alt="profile-avatar" title="other-users-avatar" class="reply-avatar" />
+                <label>
+                    <a href="#" class="reply-name">
+                        V.T
+                    </a>
+                </label>
+            <textarea placeholder="Write a comment..." class="reply-text"></textarea>
+        </div>
     </article>`
------------------------------------------------------------------------------------------*/
-    
-    homeFeeds.insertBefore(article, homeFeeds.firstChild);
-    inputQuestion.value = "";
+-------------------------------------------------------------------------------------------------*/
 }
 
 /*---Function that can post questions from anywhere on the page throught the nav menu----*/
@@ -188,7 +248,7 @@ const postUsersQuestion2 = () => {
     /*--------------------------------------------------------------------------------------*/
     let input = document.createElement("input");
     let type = document.createAttribute("type");
-    type.value = "button";
+    type.value = "submit";
     input.setAttributeNode(type);
 
     let val = document.createAttribute("value");
@@ -207,7 +267,7 @@ const postUsersQuestion2 = () => {
     title.value = "delete a post";
     input.setAttributeNode(title);
 
-    //----------------------------------------------
+    // //------------------------------
 
     let input2 = document.createElement("input");
     let type2 = document.createAttribute("type");
@@ -226,6 +286,10 @@ const postUsersQuestion2 = () => {
     id2.value = "edit-button";
     input2.setAttributeNode(id2);
 
+    let title2 = document.createAttribute("title");
+    title2.value = "update a post";
+    input2.setAttributeNode(title2);
+
     // //------------------------------
 
     let input3 = document.createElement("input");
@@ -239,7 +303,7 @@ const postUsersQuestion2 = () => {
 
     let _class1 = document.createAttribute("class");
     _class1.value = "comment";
-    input3.setAttributeNode(_class1); //title="reply a post"
+    input3.setAttributeNode(_class1);
 
     let id3 = document.createAttribute("id");
     id3.value = "comment-button";
@@ -253,9 +317,59 @@ const postUsersQuestion2 = () => {
     let inputPost = `${inputQuestion.value}` || `${postTopQuestion.value}`;
     let text = document.createTextNode(inputPost);
     paragraph.appendChild(text);
+
+    paragraph.style.marginBottom = "1.5em";
     /*--------------------------------------------------------------------------------------*/
     label.appendChild(anchor); //Label appends the anchor
+
     /*--------------------------------------------------------------------------------------*/
+    /*-----------------------------------------COMMENT--------------------------------------*/
+
+    let divc = document.createElement("div");
+    let _classc = document.createAttribute("class");
+    _classc.value = "comment-main-container";
+    divc.setAttributeNode(_classc);
+
+    let imgc = document.createElement("img");
+    let srcc = document.createAttribute("src");
+    srcc.value = "images/avatar.png";
+    imgc.setAttributeNode(srcc);
+
+    let _class1c = document.createAttribute("class");
+    _class1c.value = "reply-avatar";
+    imgc.setAttributeNode(_class1c);
+
+    let labelc = document.createElement("label");
+
+    let anchorc = document.createElement("a");
+    let linkc = document.createAttribute("href");
+    linkc.value = "#";
+    anchorc.setAttributeNode(linkc);
+
+    let _class2c = document.createAttribute("class");
+    _class2c.value = "reply-name";
+    anchorc.setAttributeNode(_class2c);
+
+    let textc = document.createTextNode("C.V");
+    anchorc.appendChild(textc);   //appended V.T to anchor
+    labelc.appendChild(anchorc); //appended anchor to label
+
+    let textAreac = document.createElement("textarea");
+    let placeHolderc = document.createAttribute("placeholder");
+    placeHolderc.value = "Write a comment...";
+    textAreac.setAttributeNode(placeHolderc);
+
+    let _class3c = document.createAttribute("class");
+    _class3c.value = "reply-text";
+    textAreac.setAttributeNode(_class3c);
+
+    divc.appendChild(imgc);
+    divc.appendChild(labelc);
+    divc.appendChild(textAreac);
+
+    /*--------------------------------------------------------------------------------------*/
+    /*--------------------------------------------------------------------------------------*/
+
     //Appends all child elements of article... 
     article.appendChild(img);
     article.appendChild(label);
@@ -263,87 +377,36 @@ const postUsersQuestion2 = () => {
     article.appendChild(input);
     article.appendChild(input2);
     article.appendChild(input3);
-
-    /*----------------------------------------------FORMAT OF ARRANGMENT------------------------------------------------- 
-        `<article class="my-post">
-            <img src="images/avatar.png" alt="profile-avatar" title="other-users-avatar" class="other-users-avatar" />
-            <label>
-                <a href="#"> Victoria Chidinmma</a>
-            </label>
-            <p>
-                ${inputQuestion.value}
-            </p>
-            <input type="button" value="Delete" class="delete-question" id="delete-button"/>
-        </article>`
-    -------------------------------------------------------------------------------------------------------------------*/
+    article.appendChild(document.createElement("hr"));
+    article.appendChild(divc);
 
     homeFeeds.insertBefore(article, homeFeeds.firstChild); //Insert the post/article inside the div but before the div's first child
-    postTopQuestion.value = "";
+    postTopQuestion.value = ""; //clears the post field after posting
+
+/*--------------------------------------FORMAT OF ARRANGMENT------------------------------------- 
+    `<article class="my-post">
+        <img src="images/avatar.png" alt="profile-avatar" title="other-users-avatar" class="other-users-avatar" />
+        <label>
+            <a href="#"> Victoria Chidinmma</a>
+        </label>
+        <p>
+            ${inputQuestion.value}
+        </p>
+        <input type="button" value="Delete" class="delete-question" id="delete-button"/>
+
+        <div class="comment-main-container">
+            <img src="images/avatar.png" alt="profile-avatar" title="other-users-avatar" class="reply-avatar" />
+                <label>
+                    <a href="#" class="reply-name">
+                        V.T
+                    </a>
+                </label>
+            <textarea placeholder="Write a comment..." class="reply-text"></textarea>
+        </div>
+    </article>`
+-------------------------------------------------------------------------------------------------*/
 }
 
-postTopButton.addEventListener("click", postUsersQuestion2);
-postButton.addEventListener("click", postUsersQuestion1);
-
-
-
-
-const comment = () => {
-    let div = document.createElement("div");
-    let _class = document.createAttribute("class");
-    _class.value = "comment-main-container";
-    div.setAttributeNode(_class);
-
-    let img = document.createElement("img");
-    let src = document.createAttribute("src");
-    src.value = "images/avatar.png";
-    img.setAttributeNode(src);
-
-    let _class1 = document.createAttribute("class");
-    _class1.value = "reply-avatar";
-    img.setAttributeNode(_class1);
-
-    let label = document.createElement("label");
-
-    let anchor = document.createElement("a");
-    let link = document.createAttribute("href");
-    link.value = "#";
-    anchor.setAttributeNode(link);
-
-    let _class2 = document.createAttribute("class");
-    _class2.value = "reply-name";
-    anchor.setAttributeNode(_class2);
-
-    let text = document.createTextNode("V.T");
-    anchor.appendChild(text);   //appended V.T to anchor
-    label.appendChild(anchor); //appended anchor to label
-
-    let textArea = document.createElement("textarea");
-    let placeHolder = document.createAttribute("placeholder");
-    placeHolder.value = "Write a comment...";
-
-    let _class3 = document.createAttribute("class");
-    _class3.value = "reply-text";
-    textArea.setAttributeNode(_class3);
-
-    div.appendChild(img);
-    div.appendChild(label);
-    div.appendChild(textArea);
-
-    let backdoor = document.getElementById("bdoor");
-    backdoor.innerHTML = div;
-
-    
-
-    /*----------------------------------------------------FORMAT OF ARRANGMENT----------------------------------------------------------------
-    <div class="comment-main-container">
-                        <img src="images/avatar.png" alt="profile-avatar" title="other-users-avatar" class="reply-avatar" />
-                        <label>
-                            <a href="#" class="reply-name">
-                               V.T
-                            </a>
-                        </label>
-                        <textarea placeholder="Write a comment..." class="reply-text"></textarea>
-                    </div>
-    --------------------------------------------------------------------------------------------------------------------------------------*/
-}
+postTopButton.addEventListener("click", postUsersQuestion2); //event listeners for top post button
+postButton.addEventListener("click", postUsersQuestion1);   //event listeners for buttom post button
 
