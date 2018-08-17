b593bed96d72feb43249e3fa1099b163f264f1c6
Nwabuzor C. Obiora <nwabuzor.obiora@gmail.com>
2018-08-17 08:45:54 +0100

    updates with dependencies and datastrutsDB file name change

--- server.js
+++ server.js
@@ -6,9 +6,20 @@ import logger from "morgan";
 const app = express();
 
 // log requests to console
-app.use(logger('dev'));
+app.use(logger("dev"));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.text());
-app.use(bodyParser.json({ type: 'application/json' }));
+app.use(bodyParser.json({ type: "application/json" }));
 
+app.get("/api", (req, res) => {
+    return res.status(200).send({
+        message: "connected successfully",
+    })
+});
+
+app.all("*", (req, res) => {
+    return res.status(404).send({
+        message: "Oops! 404. Page not found",
+    })
+})
