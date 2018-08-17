0e6395377cbf6b0d95928351fdef4784821139fc
Nwabuzor C. Obiora <nwabuzor.obiora@gmail.com>
2018-08-17 08:03:01 +0100

    deleted some files

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 import express from "express";
-import bodyParser from "body-parser";
+import bodyParser from "body-parser"; //middleware that reads requests
 import logger from "morgan";
 
+//Assigning express framework to a variable to make portable
 const app = express();
+
+// log requests to console
+app.use(logger('dev'));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: 'application/json' }));
+
